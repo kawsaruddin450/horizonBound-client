6cb76d347ab06fd1247e28435771654da25772c9
kawsar-uddin <kawsaruddin450@gmail.com>
2024-09-16 19:53:57 +0600

    add login, signup, signout function in AuthProvider file

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,13 +1,45 @@
-import React, { createContext } from 'react';
-import { getAuth } from "firebase/auth";
+import { createContext, useEffect, useState } from 'react';
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
 import app from '../firebase/firebase.config'
 
 export const AuthContext = createContext();
 
 const AuthProvider = ({children}) => {
     const auth = getAuth(app);
+
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    const loginUser = (email, password) => {
+        setLoading(true);
+        return signInWithEmailAndPassword(auth, email, password);
+    }
+    const signUpUser = (email, password) => {
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
+    const logOutUser = () => {
+        setLoading(true);
+        return signOut(auth);
+    }
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, currentUser => {
+            setUser(currentUser);
+            setLoading(false);
+        })
+        return () => {
+            return unsubscribe();
+        }
+    }, [auth])
+
+
     const authInfo = {
-        user : "kawsar"
+        user,
+        loading,
+        loginUser,
+        signUpUser,
+        logOutUser,
     }
     return (
         <AuthContext.Provider value={authInfo}>
