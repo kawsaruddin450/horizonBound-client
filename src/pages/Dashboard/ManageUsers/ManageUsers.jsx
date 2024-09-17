d82286c5110b533a783e7f2167860a84fd35b444
kawsar-uddin <kawsaruddin450@gmail.com>
2024-09-18 00:22:29 +0600

    diplay all users in table in manage users page

--- src/pages/Dashboard/ManageUsers/ManageUsers.jsx
+++ src/pages/Dashboard/ManageUsers/ManageUsers.jsx
@@ -1,12 +1,14 @@
 import { useQuery } from "@tanstack/react-query";
+import { Helmet } from "react-helmet-async";
+import { FaRegTrashAlt } from "react-icons/fa";
 
 
 const ManageUsers = () => {
     const token = localStorage.getItem('access-token')
 
-    const {data: users, refetch} = useQuery({
+    const { data: users } = useQuery({
         queryKey: ['users'],
-        queryFn: async()=> {
+        queryFn: async () => {
             const response = await fetch(`http://localhost:5000/users`, {
                 headers: {
                     authorization: `bearer ${token}`
@@ -17,8 +19,68 @@ const ManageUsers = () => {
     })
     console.log(users);
     return (
-        <div>
-            <h1>Show all users: {users?.length}</h1>
+        <div className="w-full m-12">
+            <Helmet>
+                <title>Manage Users - Camp HorizonBound</title>
+            </Helmet>
+            <div className="flex items-center justify-evenly text-2xl font-semibold">
+                <h2>Total Users: {users?.length}</h2>
+                <h2>Your Role: Admin</h2>
+            </div>
+            <div className="overflow-x-auto m-12">
+                <table className="table">
+                    {/* head */}
+                    <thead className="bg-blue-500 text-white font-semibold">
+                        <tr>
+                            <th>
+                                <label>
+                                    <input type="checkbox" className="checkbox" />
+                                </label>
+                            </th>
+                            <th>Name</th>
+                            <th>Role</th>
+                            <th>Change Role</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            users.map((user, index) => <tr
+                            key={user._id}
+                            >
+                                <th>
+                                    {index + 1}
+                                </th>
+                                <td>
+                                    <div className="flex items-center gap-3">
+                                        <div className="avatar">
+                                            <div className="mask mask-squircle h-12 w-12">
+                                                <img
+                                                    src={user.image}
+                                                    alt="User Image" />
+                                            </div>
+                                        </div>
+                                        <div>
+                                            <div className="font-bold">{user.name}</div>
+                                            <div className="text-sm opacity-50">{user.email}</div>
+                                        </div>
+                                    </div>
+                                </td>
+                                <td>
+                                    {user.role}
+                                </td>
+                                <td className="space-y-3 text-center items-center">
+                                    <button className="block btn btn-xs text-white btn-accent mx-auto">Make Instructor</button>                                    
+                                    <button className="block btn btn-xs text-white btn-warning mx-auto">Make Admin</button>                                    
+                                </td>
+                                <th>
+                                    <button className="btn btn-sm text-md btn-error text-white"><FaRegTrashAlt></FaRegTrashAlt></button>
+                                </th>
+                            </tr>)
+                        }
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
