799b280f9b36b643b7b0e67a6a6c03bf84aede87
kawsar-uddin <kawsaruddin450@gmail.com>
2024-09-17 13:19:33 +0600

    save selected items to server

--- src/pages/Shared/CourseCard/CourseCard.jsx
+++ src/pages/Shared/CourseCard/CourseCard.jsx
@@ -1,7 +1,54 @@
-import PrimaryBtn from '../../../components/PrimaryBtn/PrimaryBtn'
+import { useContext } from 'react';
+import PrimaryBtn from '../../../components/PrimaryBtn/PrimaryBtn';
+import { AuthContext } from '../../../providers/AuthProvider';
+import Swal from 'sweetalert2';
+import { useLocation, useNavigate } from 'react-router-dom';
 
-const CourseCard = ({course}) => {
-    const {name, description, instructor, price, available_seats, image, category} = course;
+const CourseCard = ({ course }) => {
+    const { user } = useContext(AuthContext);
+    const { _id, name, description, instructor, price, available_seats, image, category } = course;
+    const location = useLocation();
+    const navigate = useNavigate();
+
+
+    const handleSelect = () => {
+        const selectedItem = { courseId: _id, name, instructor, price, available_seats, image, category, email: user?.email };
+
+        if (user) {
+            fetch(`http://localhost:5000/courses`, {
+                method: "POST",
+                headers: {
+                    "content-type": "application/json"
+                },
+                body: JSON.stringify(selectedItem)
+            })
+                .then(res => res.json())
+                .then(data => {
+                    if (data.insertedId) {
+                        Swal.fire({
+                            title: "Congratulations!",
+                            text: "This course have been selected successfully.",
+                            icon: "success"
+                        });
+                    }
+                })
+        }
+        else {
+            Swal.fire({
+                title: "Sorry!",
+                text: "You have to login first.",
+                icon: "warning",
+                showCancelButton: true,
+                confirmButtonColor: "#3085d6",
+                cancelButtonColor: "#d33",
+                confirmButtonText: "Go to Login"
+              }).then((result) => {
+                if (result.isConfirmed) {
+                  navigate('/login', {state: {from: location}});
+                }
+              });
+        }
+    }
     return (
         <div className="card bg-base-100 shadow-xl">
             <figure>
@@ -16,7 +63,7 @@ const CourseCard = ({course}) => {
                 <h3 className='text-xl mt-5'>Instructor: <span className='font-bold'>{instructor}</span></h3>
                 <h3 className='text-xl'>Available Seats: {available_seats}</h3>
                 <div className="card-actions justify-start mt-5">
-                    <PrimaryBtn className="btn btn-primary">Select Now</PrimaryBtn>
+                    <button onClick={handleSelect} className="btn btn-secondary rounded-3xl">Select Now</button>
                 </div>
             </div>
         </div>
